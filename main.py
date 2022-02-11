3b7d33942a6e5ef7a3499939bf4a4737488ca68c
Luka <lucca.v.rocha@gmail.com>
2022-02-11 13:56:48 -0300

    Check if position is valid for number

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from operator import le
-from webbrowser import get
-
-
 Board = [
     [0,0,0,0,0,0,0,0,0],
     [5,9,0,0,3,4,6,0,0],
@@ -13,6 +9,27 @@ Board = [
     [3,0,0,8,0,0,2,6,0],
     [0,5,0,0,7,0,0,0,0]
 ]
+
+def valid(Board, n, pos):
+	#row
+	for i in range(len(Board[0])):
+		if Board[pos[0]][i] == n and pos[1] != i:
+			return False
+	
+	#column
+	for i in range(len(Board)):
+		if Board[i][pos[1]] == n and pos[1] != i:
+			return False
+	#Check box
+	Caixa_x = pos[1] // 3
+	Caixa_y = pos[0] // 3
+
+	for i in range(Caixa_x* 3, Caixa_x*3 +3):
+		for j in range(Caixa_y* 3, Caixa_y*3 +3):
+			if Board[i][j] == n and (i,j) != pos:
+				return False
+	return True
+				
 
 def printBoard(Board):
 	for i in range(len(Board)):
